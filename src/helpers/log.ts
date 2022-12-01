bc6e70b8d61b91b9dc3eb490628beaac2a56a34e
bonustrack <fabien@bonustrack.co>
2022-12-01 12:36:52 +0700

    Update based on hub latest changes

--- src/helpers/log.ts
+++
@@ -1,9 +0,0 @@
-import winston from 'winston';
-
-const log = winston.createLogger({
-  transports: new winston.transports.Console({
-    format: winston.format.combine(winston.format.colorize(), winston.format.simple())
-  })
-});
-
-export default log;
