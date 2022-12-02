fe7c1a7ad203c77f513b72a159219f922baf6619
bonustrack <fabien@bonustrack.co>
2022-12-03 06:37:37 +0700

    refactor: remove api prefix

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,8 @@ app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ limit: '20mb', extended: false }));
 app.use(cors({ maxAge: 86400 }));
 app.set('trust proxy', 1);
-app.use('/api', api);
-app.use('/api/shutter', shutter);
-app.get('/*', (req, res) => res.redirect('/api'));
+app.use('/', api);
+app.use('/shutter', shutter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => log.info(`Started on: http://localhost:${PORT}`));
