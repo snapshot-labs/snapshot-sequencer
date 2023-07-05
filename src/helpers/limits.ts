db412111345d7afbdd0713cd51ea55cb7e449b9f
less <fabien@bonustrack.co>
2023-07-05 14:01:47 +0700

    fix order

--- src/helpers/limits.ts
+++ src/helpers/limits.ts
@@ -14,16 +14,16 @@ export const ECOSYSTEM_SPACE_PROPOSAL_MONTH_LIMIT = 1000;
 export const ECOSYSTEM_SPACES = ['orbapp.eth', 'cakevote.eth'];
 
 export function getSpaceLimits(space): number[] {
-  if (verifiedSpaces.includes(space)) {
-    return [VERIFIED_SPACE_PROPOSAL_DAY_LIMIT, VERIFIED_SPACE_PROPOSAL_MONTH_LIMIT];
-  }
-
   if (flaggedSpaces.includes(space)) {
     return [FLAGGED_SPACE_PROPOSAL_DAY_LIMIT, FLAGGED_SPACE_PROPOSAL_MONTH_LIMIT];
   }
 
   if (ECOSYSTEM_SPACES.includes(space)) {
     return [ECOSYSTEM_SPACE_PROPOSAL_DAY_LIMIT, ECOSYSTEM_SPACE_PROPOSAL_MONTH_LIMIT];
+  }
+
+  if (verifiedSpaces.includes(space)) {
+    return [VERIFIED_SPACE_PROPOSAL_DAY_LIMIT, VERIFIED_SPACE_PROPOSAL_MONTH_LIMIT];
   }
 
   return [SPACE_PROPOSAL_DAY_LIMIT, SPACE_PROPOSAL_MONTH_LIMIT];
