e353b53dbb31951ea12fa2030d449eaec8a04691
Chaitanya <yourchaitu@gmail.com>
2023-07-19 23:06:38 +0530

    Add moonbeam-free-balance to override strategies to avoid outdated VP on existing votes

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -56,7 +56,8 @@ export function hasStrategyOverride(strategies: any[]) {
     '"erc20-votes-with-override"',
     '"esd-delegation"',
     '"ocean-dao-brightid"',
-    '"orbs-network-delegation"'
+    '"orbs-network-delegation"',
+    '"moonbeam-free-balance"'
   ];
   const strategiesStr = JSON.stringify(strategies).toLowerCase();
   return keywords.some(keyword => strategiesStr.includes(`"name":${keyword}`));
