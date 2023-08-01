f6c34740e335ef392ad87606bad72719acfcb057
Wan <495709+wa0x6e@users.noreply.github.com>
2023-08-01 18:52:29 +0900

    fix: pull flagged items from online database (#101)

--- src/helpers/moderation.ts
+++ src/helpers/moderation.ts
@@ -6,12 +6,18 @@ const moderationURL = 'https://sh5.co/api/moderation';
 
 export let flaggedSpaces: Array<string> = [];
 export let flaggedIps: Array<string> = [];
+export let flaggedAddresses: Array<string> = [];
+export let flaggedProposalTitleKeywords: Array<string> = [];
+export let flaggedProposalBodyKeywords: Array<string> = [];
 export let verifiedSpaces: Array<string> = [];
 
 async function loadModerationData() {
   const res = await snapshot.utils.getJSON(moderationURL);
   flaggedSpaces = res?.flaggedSpaces;
   flaggedIps = res?.flaggedIps;
+  flaggedAddresses = res?.flaggedAddresses;
+  flaggedProposalTitleKeywords = res?.flaggedProposalTitleKeywords;
+  flaggedProposalBodyKeywords = res?.flaggedProposalBodyKeywords;
   verifiedSpaces = res?.verifiedSpaces;
 }
 
