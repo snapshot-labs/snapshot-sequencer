9a1ddcc48de51f21d63907cc05941c3566b8741f
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-08-12 00:57:55 +0900

    chore: update schema for test database

--- test/schema.sql
+++ test/schema.sql
@@ -142,3 +142,25 @@ CREATE TABLE statements (
   INDEX created (created),
   INDEX updated (updated)
 );
+
+CREATE TABLE messages (
+  mci INT NOT NULL AUTO_INCREMENT,
+  id VARCHAR(66) NOT NULL,
+  ipfs VARCHAR(64) NOT NULL,
+  address VARCHAR(64) NOT NULL,
+  version VARCHAR(6) NOT NULL,
+  timestamp BIGINT NOT NULL,
+  space VARCHAR(64),
+  type VARCHAR(24) NOT NULL,
+  sig VARCHAR(256) NOT NULL,
+  receipt VARCHAR(256) NOT NULL,
+  PRIMARY KEY (id),
+  INDEX mci (mci),
+  INDEX ipfs (ipfs),
+  INDEX address (address),
+  INDEX version (version),
+  INDEX timestamp (timestamp),
+  INDEX space (space),
+  INDEX type (type),
+  INDEX receipt (receipt)
+);
