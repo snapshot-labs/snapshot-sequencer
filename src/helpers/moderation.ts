20a03190105027bfd0f209e6bdd608c0bee601a5
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-08-11 20:29:04 +0900

    chore: fix merge

--- src/helpers/moderation.ts
+++ src/helpers/moderation.ts
@@ -11,13 +11,6 @@ export let flaggedAddresses: Array<string> = [];
 export let flaggedProposalTitleKeywords: Array<string> = [];
 export let flaggedProposalBodyKeywords: Array<string> = [];
 export let verifiedSpaces: Array<string> = [];
-export const flaggedIps: Array<string> = [
-  '594c3796d3e139686d85fdfd48f58eb27748703689e93ac9404f8a6e3fe69488',
-  'f38f87bfd58860fdb0dac0374ee6e1f4ef823867cd01286de4b031d762ceb18e',
-  '516263be80d8ec183d89dbedf8093852775ed38ad2e2fff03f018522247651bd',
-  'aed8ab2423772377b19170381d72d1d7b85bc741bc77700c0ff14c3e081e3605',
-  '7d85f3c5a23d9773662ab276a04f064ed406215315a550dc337cf4276c22a747'
-];
 
 async function loadModerationData() {
   const res = await snapshot.utils.getJSON(moderationURL);
