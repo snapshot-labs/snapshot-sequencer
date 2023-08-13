0362a41b3d7d19d7c5c8114bc685852ab4a1eadd
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-08-13 15:30:11 +0900

    feat: add metrics

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import 'dotenv/config';
 import cors from 'cors';
+import { initLogger, fallbackLogger } from '@snapshot-labs/snapshot-sentry';
 import express from 'express';
 import api from './api';
 import shutter from './helpers/shutter';
 import log from './helpers/log';
-import { initLogger, fallbackLogger } from '@snapshot-labs/snapshot-sentry';
+import initMetrics from './helpers/metrics';
 
 const app = express();
 
 initLogger(app);
+initMetrics(app);
 
+app.disable('x-powered-by');
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ limit: '20mb', extended: false }));
 app.use(cors({ maxAge: 86400 }));
