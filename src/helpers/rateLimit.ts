66fb616f787cdde37fc6adb83572d52c7abed4e0
ChaituVR <yourchaitu@gmail.com>
2023-08-26 02:26:37 +0530

    Redis store to rate limit

--- src/helpers/rateLimit.ts
+++ src/helpers/rateLimit.ts
@@ -1,6 +1,23 @@
 import rateLimit from 'express-rate-limit';
+import RedisStore from 'rate-limit-redis';
+import { createClient } from 'redis';
 import { getIp, sendError } from './utils';
 import log from './log';
+
+let client;
+
+(async () => {
+  if (!process.env.RATE_LIMIT_DATABASE_URL) return;
+
+  console.log('[redis-rl] Connecting to Redis');
+  client = createClient({ url: process.env.RATE_LIMIT_DATABASE_URL });
+  client.on('connect', () => console.log('[redis-rl] Redis connect'));
+  client.on('ready', () => console.log('[redis-rl] Redis ready'));
+  client.on('reconnecting', err => console.log('[redis-rl] Redis reconnecting', err));
+  client.on('error', err => console.log('[redis-rl] Redis error', err));
+  client.on('end', err => console.log('[redis-rl] Redis end', err));
+  await client.connect();
+})();
 
 export default rateLimit({
   windowMs: 60 * 1e3,
@@ -11,5 +28,11 @@ export default rateLimit({
   handler: (req, res) => {
     log.info(`too many requests ${getIp(req).slice(0, 7)}`);
     sendError(res, 'too many requests', 429);
-  }
+  },
+  store: client
+    ? new RedisStore({
+        sendCommand: (...args: string[]) => client.sendCommand(args),
+        prefix: 'hub:'
+      })
+    : undefined
 });
