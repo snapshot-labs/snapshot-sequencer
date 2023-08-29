63bdcec334911167a71cbdc7723e49e5a12beb3d
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-08-29 23:55:37 +0900

    chore: instrument rate limited request

--- src/helpers/metrics.ts
+++ src/helpers/metrics.ts
@@ -1,14 +1,33 @@
 import init, { client } from '@snapshot-labs/snapshot-metrics';
 import { Express } from 'express';
 
+const whitelistedPath = [/^\/$/, /^\/scores\/.+$/, /^\/spaces\/.+\/poke$/];
+
+const rateLimitedRequestsCount = new client.Counter({
+  name: 'http_requests_by_rate_limit_count',
+  help: 'Total number of requests, by rate limit status',
+  labelNames: ['rate_limited']
+});
+
+function instrumentRateLimitedRequests(req, res, next) {
+  res.on('finish', () => {
+    if (whitelistedPath.some(path => path.test(req.path))) {
+      rateLimitedRequestsCount.inc({ rate_limited: res.statusCode === 429 ? 1 : 0 });
+    }
+  });
+
+  next();
+}
+
 export default function initMetrics(app: Express) {
   init(app, {
     normalizedPath: [
       ['/scores/.+', '/scores/#id'],
       ['/spaces/.+/poke', '/spaces/#key/poke']
     ],
-    whitelistedPath: [/^\/$/, /^\/scores\/.+$/, /^\/spaces\/.+\/poke$/]
+    whitelistedPath
   });
+  app.use(instrumentRateLimitedRequests);
 }
 
 export const timeIngestorProcess = new client.Histogram({
