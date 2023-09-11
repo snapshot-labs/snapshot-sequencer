cf6635e930a7722e10a298afc5fe272512c97291
Dmytro Tolok <tolok.d@gmail.com>
2023-09-11 13:33:42 +0200

    fix(comment): added extra interpolation to the error message

--- src/api.ts
+++ src/api.ts
@@ -21,7 +21,7 @@ router.post('/', async (req, res) => {
     const result = await typedData(req);
     return res.json(result);
   } catch (e) {
-    log.warn(`[ingestor] msg validation failed (typed data): ${e}`);
+    log.warn(`[ingestor] msg validation failed (typed data): ${e} (${JSON.stringify(e)})`);
     return sendError(res, e);
   }
 });
