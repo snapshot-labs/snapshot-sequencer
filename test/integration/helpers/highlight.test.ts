e3a50f0636eba0f74bd18f556dd6dbc6bbe724e1
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-09-25 02:58:48 +0900

    fix: remove commented test

--- test/integration/helpers/highlight.test.ts
+++ test/integration/helpers/highlight.test.ts
@@ -3,22 +3,21 @@ import db from '../../../src/helpers/mysql';
 
 describe('highlight', () => {
   describe('isDuplicateMsg()', () => {
-    // afterAll(async () => {
-    //   await db.queryAsync(
-    //     'DELETE from snapshot_sequencer_test.messages where sig = ?',
-    //     'test-exists'
-    //   );
-    //   return db.endAsync();
-    // });
+    afterAll(async () => {
+      await db.queryAsync(
+        'DELETE from snapshot_sequencer_test.messages where sig = ?',
+        'test-exists'
+      );
+      return db.endAsync();
+    });
 
     it('returns false when message does not exist yet', async () => {
-      expect(1).toEqual(1);
-      // expect(await isDuplicateMsg('test-not-exists')).toEqual(false);
+      expect(await isDuplicateMsg('test-not-exists')).toEqual(false);
     });
 
     it('returns true when message already exist', async () => {
-      // await storeMsg('', '', '', '', '', '', '', 'test-exists', '');
-      // expect(await isDuplicateMsg('test-exists')).toEqual(true);
+      await storeMsg('', '', '', '', '', '', '', 'test-exists', '');
+      expect(await isDuplicateMsg('test-exists')).toEqual(true);
     });
   });
 });
