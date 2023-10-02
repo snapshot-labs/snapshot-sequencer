a9f5d583fa98e6f71d68142c90d7003fc89b86ac
Dmytro Tolok <tolok.d@gmail.com>
2023-10-02 13:15:00 +0200

    feat(flag): trigger verified/flagged field on set opposite data (#185)
    * feat(flag): trigger verified/flagged field on set opposite data
    
    * Update src/helpers/moderation.ts
    
    * Update src/helpers/moderation.ts
    
    * fix(sql): fix queries for space/proposal flagging
    
    ---------
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>

--- src/helpers/moderation.ts
+++ src/helpers/moderation.ts
@@ -44,10 +44,10 @@ export function flagEntity({ type, action, value }) {
   let query;
   switch (`${type}-${action}`) {
     case 'space-flag':
-      query = `UPDATE spaces SET flagged = 1 WHERE id = ? LIMIT 1`;
+      query = `UPDATE spaces SET flagged = 1, verified = 0 WHERE id = ? LIMIT 1`;
       break;
     case 'space-verify':
-      query = `UPDATE spaces SET verified = 1 WHERE id = ? LIMIT 1`;
+      query = `UPDATE spaces SET verified = 1, flagged = 0 WHERE id = ? LIMIT 1`;
       break;
     case 'proposal-flag':
       query = `UPDATE proposals SET flagged = 1 WHERE id = ? LIMIT 1`;
