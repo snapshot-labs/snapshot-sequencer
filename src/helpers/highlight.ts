9ccd95e5d231fb7ac80232fa45f083340f53e820
ChaituVR <yourchaitu@gmail.com>
2023-10-04 23:38:50 +0530

    feat: Store message on sequencer DB

--- src/helpers/highlight.ts
+++ src/helpers/highlight.ts
@@ -1,8 +1,8 @@
-import db from './mysql';
+import { hubDB, sequencerDB } from './mysql';
 
 export async function storeMsg(id, ipfs, address, version, timestamp, space, type, sig, receipt) {
   const query = 'INSERT IGNORE INTO messages SET ?';
-  await db.queryAsync(query, [
+  const result = await hubDB.queryAsync(query, [
     {
       id,
       ipfs,
@@ -15,4 +15,20 @@ export async function storeMsg(id, ipfs, address, version, timestamp, space, typ
       receipt
     }
   ]);
+  if (result.insertId) {
+    await sequencerDB.queryAsync(query, [
+      {
+        id,
+        mci: result.insertId,
+        ipfs,
+        address,
+        version,
+        timestamp,
+        space,
+        type,
+        sig,
+        receipt
+      }
+    ]);
+  }
 }
