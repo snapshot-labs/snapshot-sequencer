6dd4577c68948faddea147960ea5292e9fdea7aa
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-10-05 00:33:55 +0900

    chore: use log.warn for consistency

--- src/writer/vote.ts
+++ src/writer/vote.ts
@@ -185,7 +185,7 @@ export async function action(body, ipfs, receipt, id, context): Promise<void> {
     if (!result) log.warn(`[writer] updateProposalAndVotes() false, ${proposalId}`);
   } catch (e) {
     capture(e, { contexts: { input: { space: msg.space, id: proposalId } } });
-    log.error(`[writer] updateProposalAndVotes() failed, ${msg.space}, ${proposalId}`);
     console.log('[writer] updateProposalAndVotes() failed', e);
+    log.warn(`[writer] updateProposalAndVotes() failed, ${msg.space}, ${proposalId}`);
   }
 }
