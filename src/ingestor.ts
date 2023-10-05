68bc19181c5693409912160363528b810122b0df
ChaituVR <yourchaitu@gmail.com>
2023-10-05 12:16:29 +0530

    fix: Use alias schema

--- src/ingestor.ts
+++ src/ingestor.ts
@@ -170,7 +170,7 @@ export default async function ingestor(req) {
     };
     const msg = jsonParse(legacyBody.msg);
 
-    if (['follow', 'unfollow', 'alias', 'subscribe', 'unsubscribe', 'profile'].includes(type)) {
+    if (['follow', 'unfollow', 'subscribe', 'unsubscribe', 'profile'].includes(type)) {
       legacyBody = message;
     }
 
