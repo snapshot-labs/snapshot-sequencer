8edc2407ee831aafdff30e25364efd73c0648102
Chaitanya <yourchaitu@gmail.com>
2023-10-11 18:04:09 +0530

    feat: Use only sequencer DB for messages (#200)
    * Use only sequencer DB for messages
    
    * chore: fix tests
    
    ---------
    
    Co-authored-by: Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>

--- src/helpers/highlight.ts
+++ src/helpers/highlight.ts
@@ -1,9 +1,8 @@
-import { default as hubDB, sequencerDB } from './mysql';
+import { sequencerDB } from './mysql';
 
 export async function storeMsg(id, ipfs, address, version, timestamp, space, type, sig, receipt) {
   const query = 'INSERT INTO messages SET ?';
-  // TODO: remove this once migration is done
-  const result = await hubDB.queryAsync(query, [
+  await sequencerDB.queryAsync(query, [
     {
       id,
       ipfs,
@@ -16,23 +15,6 @@ export async function storeMsg(id, ipfs, address, version, timestamp, space, typ
       receipt
     }
   ]);
-  if (result.insertId) {
-    // TODO: remove IGNORE once migration is done
-    await sequencerDB.queryAsync('INSERT IGNORE INTO messages SET ?', [
-      {
-        id,
-        mci: result.insertId,
-        ipfs,
-        address,
-        version,
-        timestamp,
-        space,
-        type,
-        sig,
-        receipt
-      }
-    ]);
-  }
 }
 
 export async function doesMessageExist(id: string): Promise<boolean> {
