d599255bad31c2b0828755e402acebe4ac9dc148
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-10-12 13:04:37 +0900

    chore: ensure connection close is triggered

--- test/setup.ts
+++ test/setup.ts
@@ -1,16 +1,17 @@
-import { default as db, sequencerDB } from '../src/helpers/mysql';
+import db, { sequencerDB } from '../src/helpers/mysql';
 
 const setup = async () => {
   try {
     await db.queryAsync('SELECT 1 + 1');
     await sequencerDB.queryAsync('SELECT 1 + 1');
-    await db.endAsync();
-    await sequencerDB.endAsync();
   } catch (e: any) {
     if (e.code === 'ER_BAD_DB_ERROR') {
       console.error('Test database not setup, please run `yarn test:setup`');
       throw new Error('Test database not setup');
     }
+  } finally {
+    await db.endAsync();
+    await sequencerDB.endAsync();
   }
 };
 
