dbe73a0299b5131be68da3a4a9072ae63f4735bd
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-10-15 12:24:43 +0900

    fix: rename column to `hibernated`

--- src/helpers/hibernation.ts
+++ src/helpers/hibernation.ts
@@ -4,7 +4,7 @@ export default function hibernate(id: string, action: string) {
   if (!id || !action) throw new Error(`missing params. 'id' and 'action' required`);
   if (!['hibernate', 'reactivate'].includes(action)) throw new Error('invalid action');
 
-  const query = `UPDATE spaces SET hibernating = ? WHERE id = ? LIMIT 1`;
+  const query = `UPDATE spaces SET hibernated = ? WHERE id = ? LIMIT 1`;
 
   return db.queryAsync(query, [action === 'hibernate' ? '1' : '0', id]);
 }
