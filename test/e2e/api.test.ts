881e843e64718793134901dee2a85ee474166490
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-10-17 16:55:29 +0900

    chore: remove extra dependencies

--- test/e2e/api.test.ts
+++ test/e2e/api.test.ts
@@ -1,4 +1,4 @@
-import request from 'supertest';
+import fetch from 'node-fetch';
 import proposalInput from '../fixtures/ingestor-payload/proposal.json';
 
 const HOST = `http://localhost:${process.env.PORT || 3003}`;
@@ -6,7 +6,7 @@ const HOST = `http://localhost:${process.env.PORT || 3003}`;
 describe('POST /', () => {
   describe('on invalid client input', () => {
     it('returns a 400 error', async () => {
-      const result = await request(HOST).post('/').send(proposalInput);
+      const result = await fetch(HOST, { method: 'POST', body: JSON.stringify(proposalInput) });
 
       expect(result.status).toBe(400);
       expect(result.body.error).toBe('client_error');
