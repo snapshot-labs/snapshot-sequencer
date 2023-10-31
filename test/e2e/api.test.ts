ff30217e3ba727f8a6b719c7391ec145f2bc9ab6
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-10-31 17:10:01 +0700

    chore: add tests

--- test/e2e/api.test.ts
+++ test/e2e/api.test.ts
@@ -1,21 +1,70 @@
 import fetch from 'node-fetch';
 import proposalInput from '../fixtures/ingestor-payload/proposal.json';
+import redis from '../../src/helpers/redis';
 
 const HOST = `http://localhost:${process.env.PORT || 3003}`;
 
+function successRequest() {
+  return fetch(`${HOST}`);
+}
+
+function failRequest() {
+  return fetch(HOST, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(proposalInput)
+  });
+}
+
 describe('POST /', () => {
+  beforeEach(async () => {
+    await redis.del('snapshot-sequencer-test:rl:3e48ef9');
+    await redis.del('snapshot-sequencer-test:rl-spam:3e48ef9');
+  });
+
+  afterAll(async () => {
+    await redis.quit();
+  });
+
   describe('on invalid client input', () => {
     it('returns a 400 error', async () => {
-      const response = await fetch(HOST, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(proposalInput)
-      });
+      const response = await failRequest();
       const body = await response.json();
 
       expect(response.status).toBe(400);
       expect(body.error).toBe('client_error');
       expect(body.error_description).toBe('wrong timestamp');
+    });
+  });
+
+  describe('rate limit', () => {
+    describe('on a mix of success and failed requests', () => {
+      it('should return a 429 errors only after 100 requests / min', async () => {
+        for (let i = 1; i <= 100; i++) {
+          // 10% of failing requests
+          const response = await (Math.random() < 0.1 ? failRequest() : successRequest());
+          expect(response.status).not.toEqual(429);
+        }
+
+        const response = await fetch(HOST, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(proposalInput)
+        });
+        expect(response.status).toBe(429);
+      });
+    });
+
+    describe('on multiple failed requests', () => {
+      it('should return a 429 errors after 15 requests / 15s', async () => {
+        for (let i = 1; i <= 15; i++) {
+          const response = await failRequest();
+          expect(response.status).toBe(400);
+        }
+
+        const response = await fetch(`${HOST}/scores/proposal-id`);
+        expect(response.status).toBe(429);
+      });
     });
   });
 });
