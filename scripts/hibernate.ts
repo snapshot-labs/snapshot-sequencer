1f9b51e1eff5092c217deea3821e69ed517a5da4
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-11-13 21:27:16 +0800

    fix: update hibernation script to hibernate spaces inactive for more than 1 year

--- scripts/hibernate.ts
+++ scripts/hibernate.ts
@@ -4,21 +4,19 @@ import networks from '@snapshot-labs/snapshot.js/src/networks.json';
 
 async function main() {
   if (process.argv.length < 2) {
-    console.error(`Usage: yarn ts-node scripts/hibernate.ts run|run-all|preview|preview-all`);
+    console.error(`Usage: yarn ts-node scripts/hibernate.ts run|preview`);
     return process.exit(1);
   }
 
   const [, , action] = process.argv;
-
-  const mainAction = action.split('-')[0];
 
   const commands = {
     preview: `SELECT COUNT(id) as count from toHibernate`,
     run: `UPDATE spaces SET hibernated = 1 where id IN (id)`
   };
 
-  if (!commands[mainAction]) {
-    console.error(`First argument should be either "run", "rnu-all", "preview" or "preview-all"`);
+  if (!commands[action]) {
+    console.error(`First argument should be either "run" or "preview"`);
     return process.exit(1);
   }
 
@@ -26,66 +24,31 @@ async function main() {
     .filter((network: any) => !network.testnet)
     .map((network: any) => network.key);
 
-  const isRunAll = action.endsWith('-all');
-
   const query = `
     WITH toHibernate AS (
       WITH data AS (
         SELECT
         id,
-        created,
-        COALESCE(JSON_UNQUOTE(JSON_EXTRACT(settings, '$.network')), '') AS network,
-        settings->>'$.validation.name' as validationName,
-        settings->>'$.strategies[*].name' as strategiesName,
-        settings->>'$.filters.minScore' as filtersMinScore,
-        settings->>'$.filters.onlyMembers' as filtersOnlyMembers,
-        settings->>'$.voteValidation.name' as voteValidation,
         (SELECT MAX(end) FROM proposals WHERE space = spaces.id LIMIT 1) AS lastProposalEndDate
         FROM spaces
         WHERE hibernated = 0
       )
 
       SELECT
-        id, network, validationName, filtersMinScore, filtersOnlyMembers, strategiesName, voteValidation, lastProposalEndDate
+        id, lastProposalEndDate
         FROM data
         WHERE
-        # Filtering out spaces that never had any activities, and are older than 6 months
-        (
-          # Older than 2 months
-          created < (UNIX_TIMESTAMP() - 180 * 24 * 60 * 60)
-          # Without activities
-          AND lastProposalEndDate IS NULL
-        )
-        # Filtering out spaces that have not been active in the last 6 months
-        OR (
-          # Last activity older than 6 months
-          lastProposalEndDate < (UNIX_TIMESTAMP() - 180 * 24 * 60 * 60)
-        )
-        ${
-          isRunAll
-            ? `# Filtering out misconfigured spaces
-              OR (
-                # Filtering only spaces without activities in the past 6 months
-                lastProposalEndDate < (UNIX_TIMESTAMP() - 180 * 24 * 60 * 60)
-                AND (
-                  # Filtering out spaces using unknown networks
-                  network NOT IN ( ? )
-                  # Without proposal validation
-                  OR ((validationName IS NULL OR validationName = 'any') AND !(filtersMinScore > 0 OR filtersOnlyMembers IS TRUE))
-                  # With ticket strategy and without vote validation
-                  OR (JSON_OVERLAPS(strategiesName, JSON_ARRAY('ticket')) && (voteValidation IS NULL OR voteValidation = 'any'))
-                )
-              )`
-            : ''
-        }
+        # Filtering out spaces that have not been active in the past year
+        FROM_UNIXTIME(lastProposalEndDate) < DATE_SUB(CURRENT_DATE, INTERVAL 1 YEAR)
+
     )
 
-    ${commands[mainAction]};
+    ${commands[action]};
   `;
 
   const results = await db.queryAsync(query, liveNetworks);
 
-  if (mainAction === 'preview') {
+  if (action === 'preview') {
     console.log(`Spaces eligible for hibernation: ${results[0].count}`);
   } else {
     console.log(`${results.message}`);
