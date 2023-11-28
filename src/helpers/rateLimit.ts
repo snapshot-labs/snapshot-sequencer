e6703242eda982370d6a8f10d30225f3e416a7b5
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-11-28 15:40:17 +0400

    fix: fix leaking redisStore key expiration

--- src/helpers/rateLimit.ts
+++ src/helpers/rateLimit.ts
@@ -5,6 +5,15 @@ import { getIp, sendError, sha256 } from './utils';
 
 const hashedIp = (req): string => sha256(getIp(req)).slice(0, 7);
 
+function getStore() {
+  return redisClient
+    ? new RedisStore({
+        sendCommand: (...args: string[]) => redisClient.sendCommand(args),
+        prefix: process.env.RATE_LIMIT_KEYS_PREFIX || 'snapshot-sequencer:'
+      })
+    : undefined;
+}
+
 const rateLimitConfig = {
   standardHeaders: true,
   legacyHeaders: false,
@@ -14,27 +23,22 @@ const rateLimitConfig = {
       'too many requests, refer to https://docs.snapshot.org/tools/api/api-keys#limits',
       429
     );
-  },
-  store: redisClient
-    ? new RedisStore({
-        sendCommand: (...args: string[]) => redisClient.sendCommand(args),
-        prefix: process.env.RATE_LIMIT_KEYS_PREFIX || 'snapshot-sequencer:'
-      })
-    : undefined
+  }
 };
-
-const regularRateLimit = rateLimit({
-  keyGenerator: req => `rl:${hashedIp(req)}`,
-  windowMs: 60 * 1e3,
-  max: 100,
-  ...rateLimitConfig
-});
 
 const highErroredRateLimit = rateLimit({
   keyGenerator: req => `rl-s:${hashedIp(req)}`,
   windowMs: 15 * 1e3,
   max: 15,
   skipSuccessfulRequests: true,
+  store: getStore(),
+  ...rateLimitConfig
+});
+const regularRateLimit = rateLimit({
+  keyGenerator: req => `rl:${hashedIp(req)}`,
+  windowMs: 60 * 1e3,
+  max: 100,
+  store: getStore(),
   ...rateLimitConfig
 });
 
