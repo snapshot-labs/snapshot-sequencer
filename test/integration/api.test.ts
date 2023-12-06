4194835dd830659ed9b508e62e8d841d42f0df2e
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-12-06 17:46:22 +0700

    chore: fix tests

--- test/integration/api.test.ts
+++ test/integration/api.test.ts
@@ -1,20 +1,14 @@
 describe('api', () => {
   describe('GET spaces/:key/poke', () => {
-    it.todo(
-      'returns an error when the domain does not have a snapshot TXT record ',
-      async () => {}
-    );
-    it.todo('returns an error when the TXT record is not an url', async () => {});
-    it.todo('returns an error when the content of the TXT record is a JSON file', async () => {});
-    it.todo(
-      'returns an error when the content of the TXT record is not a valid space schema',
-      async () => {}
-    );
+    it.todo('returns an error when the domain does not have a snapshot TXT record ');
+    it.todo('returns an error when the TXT record is not an url');
+    it.todo('returns an error when the content of the TXT record is a JSON file');
+    it.todo('returns an error when the content of the TXT record is not a valid space schema');
     describe('when the space does not exist', () => {
-      it.todo('creates a new space', async () => {});
+      it.todo('creates a new space');
     });
     describe('when the space exist', () => {
-      it.todo('updates a new space', async () => {});
+      it.todo('updates a new space');
     });
   });
 });
