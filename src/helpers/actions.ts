c02575ddcd60d28b08a358231fc150649f132768
Less <bonustrack@users.noreply.github.com>
2024-02-25 11:27:10 +0700

    fix: add counters on space creation (#293)

--- src/helpers/actions.ts
+++ src/helpers/actions.ts
@@ -12,7 +12,10 @@ export async function addOrUpdateSpace(space: string, settings: any) {
       name: settings.name,
       created: ts,
       updated: ts,
-      settings: JSON.stringify(settings)
+      settings: JSON.stringify(settings),
+      proposal_count: 0,
+      vote_count: 0,
+      follower_count: 0
     },
     ts,
     JSON.stringify(settings),
