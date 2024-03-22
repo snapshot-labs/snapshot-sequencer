06776fa4bd3b0af693d023b7a1008486289cb5db
Less <bonustrack@users.noreply.github.com>
2024-03-22 18:06:00 +0700

    blockaid fix lint

--- src/writer/proposal.ts
+++ src/writer/proposal.ts
@@ -10,7 +10,7 @@ import { capture } from '@snapshot-labs/snapshot-sentry';
 import { flaggedAddresses, containsFlaggedLinks } from '../helpers/moderation';
 import { validateSpaceSettings } from './settings';
 // import { isMalicious } from '../helpers/blockaid';
-import { blockaidBlockedRequestsCount } from '../helpers/metrics';
+// import { blockaidBlockedRequestsCount } from '../helpers/metrics';
 
 const scoreAPIUrl = process.env.SCORE_API_URL || 'https://score.snapshot.org';
 const broviderUrl = process.env.BROVIDER_URL || 'https://rpc.snapshot.org';
@@ -104,6 +104,7 @@ export async function verify(body): Promise<any> {
     if (msg.payload.type !== space.voting.type) return Promise.reject('invalid voting type');
   }
 
+  /**
   try {
     const content = `
       ${msg.payload.name || ''}
@@ -111,13 +112,14 @@ export async function verify(body): Promise<any> {
       ${msg.payload.discussion || ''}
     `;
 
-    // if (await isMalicious(content)) {
-    //   blockaidBlockedRequestsCount.inc({ space: space.id });
-    //   return Promise.reject('invalid proposal content');
-    // }
+    if (await isMalicious(content)) {
+      blockaidBlockedRequestsCount.inc({ space: space.id });
+      return Promise.reject('invalid proposal content');
+    }
   } catch (e) {
     log.warning('[writer] Failed to query Blockaid');
   }
+  */
 
   if (flaggedAddresses.includes(addressLC))
     return Promise.reject('invalid proposal, please contact support');
