9495f85c8096f3836dcb612c4b458a0925a41c5f
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-03-31 20:16:50 +0400

    refactor: remove redis dependency

--- src/helpers/duplicateRequestPreventor.ts
+++ src/helpers/duplicateRequestPreventor.ts
@@ -1,39 +1,24 @@
 import type { Request, Response, NextFunction } from 'express';
 import { sendError } from './utils';
-import redisClient from './redis';
 
-const KEYS_PREFIX = process.env.RATE_LIMIT_KEYS_PREFIX || 'snapshot-sequencer:';
-export const DUPLICATOR_SET_KEY = `${KEYS_PREFIX}processing-requests`;
 export const ERROR_MESSAGE = 'request already being processed';
-
+export const queue: Set<string> = new Set();
 const hashedBody = (req: Request): string => JSON.stringify(req.body?.sig || {});
-
-redisClient.on('ready', async () => await redisClient.del(DUPLICATOR_SET_KEY));
 
 export default async function duplicateRequestPreventor(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  if (!redisClient || req.method !== 'POST' || !req.body) {
-    return next();
-  }
-
   const value = hashedBody(req);
-  const [duplicate] = await redisClient
-    .multi()
-    .sIsMember(DUPLICATOR_SET_KEY, value)
-    .sAdd(DUPLICATOR_SET_KEY, value)
-    .exec();
 
-  if (duplicate) {
-    return sendError(res, ERROR_MESSAGE, 429);
-  }
+  if (queue.has(value)) return sendError(res, ERROR_MESSAGE, 429);
 
+  queue.add(value);
   next();
 }
 
 export async function cleanup(req: Request, res: Response, next: NextFunction) {
-  await redisClient.sRem(DUPLICATOR_SET_KEY, hashedBody(req));
+  queue.delete(hashedBody(req));
   next();
 }
--- src/helpers/metrics.ts
+++ src/helpers/metrics.ts
@@ -1,8 +1,7 @@
 import init, { client } from '@snapshot-labs/snapshot-metrics';
 import { capture } from '@snapshot-labs/snapshot-sentry';
 import { Express } from 'express';
-import redisClient from './redis';
-import { DUPLICATOR_SET_KEY } from './duplicateRequestPreventor';
+import { queue } from './duplicateRequestPreventor';
 import db from './mysql';
 
 const whitelistedPath = [/^\/$/, /^\/scores\/.+$/, /^\/spaces\/.+\/poke$/];
@@ -88,6 +87,6 @@ new client.Gauge({
   name: 'duplicate_request_queue_size',
   help: 'Number of items in the duplicate requests prevention queue',
   async collect() {
-    this.set((await redisClient.sCard(DUPLICATOR_SET_KEY)) || 0);
+    this.set(queue.size);
   }
 });
