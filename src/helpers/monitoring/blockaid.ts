faa4b5b19f7821b6897b3b5d54f12124f01eb20d
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-04-13 15:02:21 +0400

    fix: use keep alive for fetch requests

--- src/helpers/monitoring/blockaid.ts
+++ src/helpers/monitoring/blockaid.ts
@@ -1,11 +1,11 @@
-import fetch from 'node-fetch';
+import { fetchWithKeepAlive } from '../utils';
 
 const BLOCKAID_API_URL = 'https://api.blockaid.io/dapp/v1/scan';
 const BLOCKAID_API_KEY = process.env.BLOCKAID_API_KEY || '';
 
 export async function scan(url: string) {
   if (!BLOCKAID_API_KEY) return { is_malicious: false };
-  const res = await fetch(BLOCKAID_API_URL, {
+  const res = await fetchWithKeepAlive(BLOCKAID_API_URL, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
