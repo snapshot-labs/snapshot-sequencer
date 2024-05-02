6bd233ffc730db7278862102468304a2e33fc0b4
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-05-02 19:23:28 +0400

    fix: fix missing char

--- test/schema.sql
+++ test/schema.sql
@@ -189,6 +189,6 @@ CREATE TABLE leaderboard (
   PRIMARY KEY user_space (user,space),
   INDEX space (space),
   INDEX vote_count (vote_count),
-  INDEX proposal_count (proposal_count)
+  INDEX proposal_count (proposal_count),
   INDEX last_vote (last_vote)
 );
