6ae6c64ae8271b4be4b46bfb6ee237f6c8c18d30
ChaituVR <yourchaitu@gmail.com>
2024-05-14 12:59:08 +0530

    Rename variables

--- src/helpers/alias.ts
+++ src/helpers/alias.ts
@@ -1,10 +1,10 @@
 import db from './mysql';
 
-export async function isValidAlias(userAddress: string, userAlias: string): Promise<boolean> {
+export async function isValidAlias(address: string, alias: string): Promise<boolean> {
   const thirtyDaysAgo = Math.floor(new Date().getTime() / 1000) - 30 * 24 * 60 * 60;
 
   const query =
     'SELECT address, alias FROM aliases WHERE address = ? AND alias = ? AND created > ? LIMIT 1';
-  const results = await db.queryAsync(query, [userAddress, userAlias, thirtyDaysAgo]);
+  const results = await db.queryAsync(query, [address, alias, thirtyDaysAgo]);
   return !!results[0];
 }
