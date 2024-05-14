cf96bc52c9f6efe480e5fdb15980301438128f2d
ChaituVR <yourchaitu@gmail.com>
2024-05-14 12:54:07 +0530

    Only use aliases that were created within the last 30 days.

--- src/helpers/alias.ts
+++ src/helpers/alias.ts
@@ -1,7 +1,10 @@
 import db from './mysql';
 
-export async function isValidAlias(from, alias): Promise<boolean> {
-  const query = 'SELECT * FROM aliases WHERE address = ? AND alias = ? LIMIT 1';
-  const results = await db.queryAsync(query, [from, alias]);
+export async function isValidAlias(userAddress: string, userAlias: string): Promise<boolean> {
+  const thirtyDaysAgo = Math.floor(new Date().getTime() / 1000) - 30 * 24 * 60 * 60;
+
+  const query =
+    'SELECT address, alias FROM aliases WHERE address = ? AND alias = ? AND created > ? LIMIT 1';
+  const results = await db.queryAsync(query, [userAddress, userAlias, thirtyDaysAgo]);
   return !!results[0];
 }
