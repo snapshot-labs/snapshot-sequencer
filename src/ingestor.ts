9acfe6dcd261819314661c30fe685cf0d37f10f3
Chaitanya <yourchaitu@gmail.com>
2024-10-29 15:21:55 +0530

    fix: Allow signatures from sepolia safes on testnet (#466)
    * fix: Allow signatures from sepolia safes on testnet
    
    * Update src/ingestor.ts

--- src/ingestor.ts
+++ src/ingestor.ts
@@ -21,7 +21,7 @@ const NETWORK_METADATA = {
     name: 'snapshot',
     version: '0.1.4',
     broviderUrl: process.env.BROVIDER_URL ?? 'https://rpc.snapshot.org',
-    defaultNetwork: '1'
+    defaultNetwork: process.env.DEFAULT_NETWORK ?? '1'
   },
   starknet: {
     name: 'sx-starknet',
