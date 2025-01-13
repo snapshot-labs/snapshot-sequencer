73dabc0e130ae01e054998ed94fa121111bfc30b
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2025-01-13 18:02:25 +0400

    refactor: remove unused function

--- scripts/import-skins.ts
+++ scripts/import-skins.ts
@@ -40,40 +40,6 @@ const SKIN_COLORS = ['primary', 'bg', 'text', 'link', 'border', 'header', 'headi
 const skins = {};
 
 /**
- * Convert HSL color to RGB
- *
- * E.g.: `hslToRgb(0.5, 0.4, 0.3)` (hsl(180, 40%, 30%)) should return [ 46, 107, 107 ]
- *
- * @param h hue, between [0,1]
- * @param s saturation, between [0,1]
- * @param l lightness, between [0,1]
- * @returns array 3-element array of RGB values, each between [0,255]
- *
- * @url https://gist.github.com/mjackson/5311256
- */
-function hslToRgb(h: number, s: number, l: number) {
-  let r: number, g: number, b: number;
-  if (s == 0) {
-    r = g = b = l; // achromatic
-  } else {
-    const hue2rgb = (p, q, t) => {
-      if (t < 0) t += 1;
-      if (t > 1) t -= 1;
-      if (t < 1 / 6) return p + (q - p) * 6 * t;
-      if (t < 1 / 2) return q;
-      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-      return p;
-    };
-    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-    const p = 2 * l - q;
-    r = hue2rgb(p, q, h + 1 / 3);
-    g = hue2rgb(p, q, h);
-    b = hue2rgb(p, q, h - 1 / 3);
-  }
-  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
-}
-
-/**
  * Convert HEX color to RGBA
  *
  * @param color hex color, 6 or 8 characters (ffffff or ffffff22)
