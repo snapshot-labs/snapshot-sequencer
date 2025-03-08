1a0903b2f90849722abaf2dabcbf2d27b93a4c53
Chaitanya <yourchaitu@gmail.com>
2025-03-08 05:51:31 +0530

    fix: accept plugins as empty string (#512)

--- src/ingestor.ts
+++ src/ingestor.ts
@@ -148,7 +148,7 @@ export default async function ingestor(req) {
         end: message.end,
         snapshot: message.snapshot,
         metadata: {
-          plugins: JSON.parse(message.plugins)
+          plugins: jsonParse(message.plugins, {})
         },
         type: message.type,
         app: message.app || ''
@@ -173,7 +173,7 @@ export default async function ingestor(req) {
         choices: message.choices,
         labels: message.labels || [],
         metadata: {
-          plugins: JSON.parse(message.plugins)
+          plugins: jsonParse(message.plugins, {})
         },
         type: message.type
       };
