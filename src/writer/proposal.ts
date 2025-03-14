94ab1c5b74f4b73fc4d6578c53cb7e322c5d32af
Chaitanya <yourchaitu@gmail.com>
2025-03-14 19:11:09 +0530

    fix: allow copeland on production (#517)
    * fix: all copeland on production
    
    * fix: remove unused variable

--- src/writer/proposal.ts
+++ src/writer/proposal.ts
@@ -10,9 +10,6 @@ import db from '../helpers/mysql';
 import { getLimits, getSpaceType } from '../helpers/options';
 import { captureError, getQuorum, jsonParse, validateChoices } from '../helpers/utils';
 
-const SNAPSHOT_ENV = process.env.NETWORK || 'testnet';
-const TESTNET_ONLY_VOTING_TYPES = ['copeland'];
-
 const scoreAPIUrl = process.env.SCORE_API_URL || 'https://score.snapshot.org';
 const broviderUrl = process.env.BROVIDER_URL || 'https://rpc.snapshot.org';
 
@@ -91,10 +88,6 @@ export async function verify(body): Promise<any> {
   const tsInt = (Date.now() / 1e3).toFixed();
   if (msg.payload.end <= tsInt) {
     return Promise.reject('proposal end date must be in the future');
-  }
-
-  if (SNAPSHOT_ENV !== 'testnet' && TESTNET_ONLY_VOTING_TYPES.includes(msg.payload.type)) {
-    return Promise.reject('voting type allowed only on testnet');
   }
 
   const isChoicesValid = validateChoices({
