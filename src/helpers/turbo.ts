b216dc7499b520d7510f38c9171fc656033ab1f9
Scott Piriou <30843220+pscott@users.noreply.github.com>
2025-03-20 15:00:58 +0100

    feat: add turbo tracking every 10s

---
+++ src/helpers/turbo.ts
@@ -0,0 +1,88 @@
+import snapshot from '@snapshot-labs/snapshot.js';
+import fetch from 'node-fetch';
+import db from './mysql';
+
+const SCHNAPS_API_URL = process.env.SCHNAPS_API_URL;
+
+const RUN_INTERVAL = 10 * 1e3; // 10 seconds
+
+// Periodically sync the turbo status of spaces with the schnaps-api
+export async function trackTurboStatuses() {
+  while (true) {
+    // Step 1: Query all the spaces from the schnaps-api
+    const spaces = await getSpacesExpirationDates();
+
+    // Step 2: Update the turbo status of the spaces in the database
+    updateTurboStatuses(spaces);
+
+    // Sleep for a while before running the loop again
+    await snapshot.utils.sleep(RUN_INTERVAL);
+  }
+}
+
+async function updateTurboStatuses(spaces: { id: string; turbo_expiration_timestamp: number }[]) {
+  if (spaces.length === 0) return;
+
+  const now = Math.floor(Date.now() / 1000); // Current timestamp in seconds
+
+  // Sync `turbo_expiration_timestamp` and `turbo` status for each space
+  const query = `
+    UPDATE spaces
+    SET
+      turbo_expiration_timestamp = CASE id
+        ${spaces.map(() => `WHEN ? THEN ?`).join(' ')}
+      END,
+      turbo = CASE
+        WHEN turbo_expiration_timestamp < ? THEN 0
+        ELSE 1
+      END
+    WHERE id IN (${spaces.map(() => '?').join(', ')});
+  `;
+
+  // Flatten `spaces` array: [id1, expiration1, id2, expiration2, ..., now, id1, id2, ...]
+  const params = spaces.flatMap(({ id, turbo_expiration_timestamp }) => [
+    id,
+    turbo_expiration_timestamp
+  ]);
+
+  // Add `now` for the `CASE` condition
+  params.push(now);
+
+  // Append `id`s again for the `WHERE IN (...)` clause
+  params.push(...spaces.map(space => space.id));
+
+  await db.query(query, params);
+}
+
+async function getSpacesExpirationDates() {
+  const query = `
+    query GetSpaces() {
+      spaces {
+        id
+        turbo_expiration_timestamp
+      }
+    }
+  `;
+
+  try {
+    const response = await fetch(SCHNAPS_API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ query })
+    });
+
+    const data = await response.json();
+
+    if (data.errors) {
+      console.error('GraphQL Errors:', data.errors);
+      return [];
+    }
+
+    return data.data.spaces;
+  } catch (error) {
+    console.error('Error fetching payments:', error);
+    return [];
+  }
+}
