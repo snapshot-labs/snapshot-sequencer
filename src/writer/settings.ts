c8121e999ea91d3cae83e6edf35a97b652f29a59
Wan <495709+wa0x6e@users.noreply.github.com>
2025-04-01 00:41:43 +0400

    feat: sync spaces custom domain with walletconnect allowed origins (#487)
    * feat: sync spaces custom domain with walletconnect allowed origins
    
    * refactor: better variable name
    
    * Update .env.example
    
    Co-authored-by: Less <bonustrack@users.noreply.github.com>
    
    * chore: only use domain without protocol prefix
    
    ---------
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>
    Co-authored-by: Less <bonustrack@users.noreply.github.com>

--- src/writer/settings.ts
+++ src/writer/settings.ts
@@ -5,7 +5,13 @@ import { addOrUpdateSpace, getSpace } from '../helpers/actions';
 import log from '../helpers/log';
 import db from '../helpers/mysql';
 import { getLimit, getSpaceType } from '../helpers/options';
-import { clearStampCache, DEFAULT_NETWORK, fetchWithKeepAlive, jsonParse } from '../helpers/utils';
+import {
+  addToWalletConnectWhitelist,
+  clearStampCache,
+  DEFAULT_NETWORK,
+  jsonParse,
+  removeFromWalletConnectWhitelist
+} from '../helpers/utils';
 
 const SNAPSHOT_ENV = process.env.NETWORK || 'testnet';
 const broviderUrl = process.env.BROVIDER_URL || 'https://rpc.snapshot.org';
@@ -153,9 +159,10 @@ export async function verify(body): Promise<any> {
 export async function action(body): Promise<void> {
   const msg = jsonParse(body.msg);
   const space = msg.space.toLowerCase();
-  const existingSettings = JSON.parse(
-    (await db.queryAsync('SELECT settings FROM spaces WHERE id = ?', [space])?.[0]) || '{}'
-  );
+  const existingSpace = (
+    await db.queryAsync('SELECT domain, settings FROM spaces WHERE id = ? LIMIT 1', [space])
+  )?.[0];
+  const existingSettings = JSON.parse(existingSpace?.settings || '{}');
 
   try {
     await addOrUpdateSpace(space, msg.payload);
@@ -166,4 +173,9 @@ export async function action(body): Promise<void> {
   }
 
   if (existingSettings.avatar !== msg.payload.avatar) await clearStampCache('space', space);
+
+  if (existingSpace?.domain != msg.payload.domain) {
+    await addToWalletConnectWhitelist(msg.payload.domain);
+    await removeFromWalletConnectWhitelist(existingSpace?.domain);
+  }
 }
