b6d764de7573e85b371b6946420285218b11dc75
pscott <30843220+pscott@users.noreply.github.com>
2025-04-15 09:33:56 +0200

    feat: add turbo tracking (#523)
    * feat: add turbo tracking every 10s
    
    * fix: correct syntax for GetSpaces query
    
    * ci: deactivate schnaps for tests
    
    * refactor: rename to turbo_expiration
    
    * chore: remove quotes around url in .env.example
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>
    
    * refactor: only track if SCHNAPS_API_URL is set
    
    * chore: remove log
    
    * chore: remove dead env var
    
    Co-authored-by: Wan <495709+wa0x6e@users.noreply.github.com>
    
    * refactor: move while loop out of if condition
    
    Co-authored-by: Wan <495709+wa0x6e@users.noreply.github.com>
    
    * lint: fix
    
    * chore: remove track turbo status
    
    * ci: move loop in if condition
    
    * refactor: move while loop out of if condition
    
    * fix: process only relevant spaces for the instance network
    
    * fix: use sentry to capture errors
    
    * fix: use fech with keep alive
    
    * fix: fix invalid comparison
    
    * docs: add note about future feature
    
    * fix: only update `turbo_expiration` field
    
    * fix: improve sql query
    
    ---------
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>
    Co-authored-by: Wan <495709+wa0x6e@users.noreply.github.com>

---
+++ src/helpers/turbo.ts
@@ -0,0 +1,95 @@
+import { capture } from '@snapshot-labs/snapshot-sentry';
+import snapshot from '@snapshot-labs/snapshot.js';
+import db from './mysql';
+import { fetchWithKeepAlive } from './utils';
+
+type Space = {
+  id: string;
+  turbo_expiration: number;
+};
+
+const SCHNAPS_API_URL = process.env.SCHNAPS_API_URL;
+const NETWORK_PREFIX = process.env.NETWORK === 'mainnet' ? 's:' : 's-tn:';
+const RUN_INTERVAL = 10 * 1e3; // 10 seconds
+
+// Periodically sync the turbo status of spaces with the schnaps-api
+export async function trackTurboStatuses() {
+  if (!SCHNAPS_API_URL) return;
+
+  while (true) {
+    // Step 1: Query all the spaces from the schnaps-api
+    let spaces = await getSpacesExpirationDates();
+
+    spaces = spaces
+      .filter(space => space.id.startsWith(NETWORK_PREFIX))
+      .map(space => {
+        return { ...space, id: space.id.replace(NETWORK_PREFIX, '') };
+      });
+
+    // Step 2: Update the turbo status of the spaces in the database
+    updateTurboStatuses(spaces);
+
+    // Sleep for a while before running the loop again
+    await snapshot.utils.sleep(RUN_INTERVAL);
+  }
+}
+
+async function updateTurboStatuses(spaces: { id: string; turbo_expiration: number }[]) {
+  if (spaces.length === 0) return;
+
+  // Sync `turbo_expiration` for each space
+  const query = `
+    UPDATE spaces
+    SET
+      turbo_expiration = CASE id
+        ${spaces.map(() => `WHEN ? THEN ?`).join(' ')}
+      END
+    WHERE id IN (?);
+  `;
+
+  // Flatten `spaces` array: [id1, expiration1, id2, expiration2, ..., now, id1, id2, ...]
+  const params: (string | string[] | number)[] = spaces.flatMap(({ id, turbo_expiration }) => [
+    id,
+    turbo_expiration
+  ]);
+
+  // Append `id`s again for the `WHERE IN (...)` clause
+  params.push(spaces.map(space => space.id));
+
+  await db.query(query, params);
+}
+
+// TODO: adds pagination to handle large number of spaces
+async function getSpacesExpirationDates(): Promise<Space[]> {
+  const query = `
+    query GetSpaces {
+      spaces {
+        id
+        turbo_expiration
+      }
+    }
+  `;
+
+  try {
+    const response = await fetchWithKeepAlive(SCHNAPS_API_URL, {
+      timeout: 5e3,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ query })
+    });
+
+    const data = await response.json();
+
+    if (data.errors) {
+      capture(data);
+      return [];
+    }
+
+    return data.data.spaces;
+  } catch (e) {
+    capture(e);
+    return [];
+  }
+}
