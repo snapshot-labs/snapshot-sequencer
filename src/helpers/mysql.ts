973da05240348883b09c1ab0042d540cc41f706d
Less <bonustrack@users.noreply.github.com>
2025-08-14 16:11:36 +0700

    chore: enable SSL with rejectUnauthorized for Hub MySQL

--- src/helpers/mysql.ts
+++ src/helpers/mysql.ts
@@ -21,6 +21,8 @@ hubConfig.connectTimeout = 60e3;
 hubConfig.acquireTimeout = 60e3;
 hubConfig.timeout = 60e3;
 hubConfig.charset = 'utf8mb4';
+hubConfig.ssl = { rejectUnauthorized: true };
+
 const hubDB = mysql.createPool(hubConfig);
 
 // @ts-ignore
@@ -34,6 +36,7 @@ sequencerConfig.connectTimeout = 60e3;
 sequencerConfig.acquireTimeout = 60e3;
 sequencerConfig.timeout = 60e3;
 sequencerConfig.charset = 'utf8mb4';
+
 const sequencerDB = mysql.createPool(sequencerConfig);
 
 export { hubDB as default, sequencerDB };
