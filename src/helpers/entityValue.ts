169e3426bbdba6453185c0e5ed04640e9a895b0c
wa0x6e <495709+wa0x6e@users.noreply.github.com>
2025-09-16 14:48:52 +0400

    fix: fix types

--- src/helpers/entityValue.ts
+++ src/helpers/entityValue.ts
@@ -24,3 +24,20 @@ export async function getStrategiesValue(proposal: Proposal): Promise<number[]> 
 
   return result.map(value => parseFloat(value.toFixed(STRATEGIES_VALUE_PRECISION)));
 }
+
+/**
+ * Calculates the total vote value based on the voting power and the proposal's value per strategy.
+ * @returns The total vote value, in the currency unit specified by the proposal's vp_value_by_strategy values
+ **/
+export function getVoteValue(proposal: { vp_value_by_strategy: number[] }, vote: Vote): number {
+  if (!proposal.vp_value_by_strategy.length) return 0;
+
+  if (proposal.vp_value_by_strategy.length !== vote.vp_by_strategy.length) {
+    throw new Error('invalid data to compute vote value');
+  }
+
+  return proposal.vp_value_by_strategy.reduce(
+    (sum, value, index) => sum + value * vote.vp_by_strategy[index],
+    0
+  );
+}
