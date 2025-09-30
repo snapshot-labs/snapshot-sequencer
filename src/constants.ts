610e24ee91c06afab17acf974cbdd7a1b38f43a4
wa0x6e <495709+wa0x6e@users.noreply.github.com>
2025-09-30 16:35:30 +0400

    fix: better constant name

--- src/constants.ts
+++ src/constants.ts
@@ -3,5 +3,5 @@ export const CB = {
   PENDING_SYNC: -10, // waiting for value from overlord
   PENDING_COMPUTE: -15, // value from overlord set, waiting for local computation or refresh
   PENDING_CLOSE: -20, // value from overlord set, waiting for proposal close for final computation
-  CLOSED: 1
+  FINAL: 1
 };
