cd6d34910dd4dfa6e1546575b201598ece4dabfd
wa0x6e <495709+wa0x6e@users.noreply.github.com>
2025-09-30 17:12:41 +0400

    fix: send request in batch

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
 export const CB = {
   INELIGIBLE: -1,
+  SYNC_ERROR: -2, // Sync error from overlord
   PENDING_SYNC: -10, // waiting for value from overlord
   PENDING_COMPUTE: -15, // value from overlord set, waiting for local computation or refresh
   PENDING_CLOSE: -20, // value from overlord set, waiting for proposal close for final computation
