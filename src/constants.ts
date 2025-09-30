93f6792a78a7a89011e4e54ebe2c4fe0e15decdc
wa0x6e <495709+wa0x6e@users.noreply.github.com>
2025-09-30 16:21:38 +0400

    fix: use static CB value for closed status

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,6 @@ export const CB = {
   INELIGIBLE: -1,
   PENDING_SYNC: -10, // waiting for value from overlord
   PENDING_COMPUTE: -15, // value from overlord set, waiting for local computation or refresh
-  PENDING_CLOSE: -20 // value from overlord set, waiting for proposal close for final computation
+  PENDING_CLOSE: -20, // value from overlord set, waiting for proposal close for final computation
+  CLOSED: 1
 };
-
-export const CURRENT_CB = parseInt(process.env.LAST_CB ?? '1');
